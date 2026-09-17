0ec1d25de2fb744f14fe86b38cb5c1efc87a093a
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2026-09-17 22:07:42 +0300

    Update Student.kt

--- app/src/main/java/com/georgehany/quickattend/data/local/entity/Student.kt
+++ app/src/main/java/com/georgehany/quickattend/data/local/entity/Student.kt
@@ -3,7 +3,6 @@ package com.georgehany.quickattend.data.local.entity
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
-import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "students",
@@ -20,7 +19,7 @@ import androidx.room.PrimaryKey
     ]
 )
 data class Student(
-    @PrimaryKey(autoGenerate = true)
+    @androidx.room.PrimaryKey(autoGenerate = true)
     val id: Long = 0,
 
     val sessionId: Long,
